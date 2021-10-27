4f69139524e6ec944edecea399ee4a5afddef424
Alex Beamer <alex@smashedcrabstudio.com>
2021-10-27 15:40:06 +0100

    ,mnb

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -20,7 +20,11 @@ namespace WebApplication4
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+
+
+                    var port = Environment.GetEnvironmentVariable("PORT");
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:" + port);
                 });
     }
 }
